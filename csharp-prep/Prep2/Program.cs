fec7395fbf3abfb20f53b4b173596743d988869c
Avimael22 <ghostriley584@gmail.com>
2024-01-12 22:24:16 -0600

    Completed the letter grade assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        float grade = float.Parse(Console.ReadLine());
+        string letter = "";
+        
+        if (grade >= 90){
+            letter = "A";
+        }
+        else if (grade >=80){
+            letter = "B";
+        }
+        else if (grade >= 70){
+            letter = "C";
+        }
+        else if (grade >= 60){
+            letter = "D";
+        }
+        else if (grade < 60){
+            letter = "F";
+        }
+        string sign = "";
+
+        if (grade%10 >= 5 && letter == "A"){
+            sign = "";
+        }
+        else if(letter == "F"){
+            sign = "";
+        }
+        else if (grade%10 >= 5){
+            sign = "+";
+        }
+        else if (grade%10 < 5){
+            sign = "-";
+        }
+        
+
+        Console.WriteLine($"With the grade you received, your corresponding letter grade is: {letter}{sign}");
+
     }
 }
