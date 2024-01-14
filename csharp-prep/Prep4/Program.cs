f2044c24c269f928961a5422d01f42fb23e722ee
Avimael22 <ghostriley584@gmail.com>
2024-01-14 13:04:02 -0600

    Finished the list assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<float> numbers = new List<float>();
+        float number;
+
+        do{
+            Console.Write("Enter a series of numbers, one at a time. Pass zero when finished: ");
+            number = float.Parse(Console.ReadLine());
+            numbers.Add(number);
+
+        }while(number != 0);
+        Console.WriteLine("Here is the list of numbers");
+
+        foreach (float num in numbers){
+            Console.WriteLine(num);
+        }
+
+        float sum = 0;
+        foreach(float num in numbers){
+            sum += num;
+        }
+        Console.WriteLine($"The sum of the numbers entered is: {sum}");
+
+        float average = sum / numbers.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        float max = 0;
+        foreach (float num in numbers){
+            if (num > max){
+                max = num;
+            }
+        }
+        Console.WriteLine($"The largest number of the numbers provided is: {max}");
     }
 }
