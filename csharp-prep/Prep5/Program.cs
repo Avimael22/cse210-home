657fe2fedd6127a1eb789e732a416cbe17c11c3f
Avimael22 <ghostriley584@gmail.com>
2024-01-14 21:52:18 -0600

    Did the functions assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string User = PromptUserName();
+        int faveNum = PromptUserNumber();
+        int squared = SquareNumber(faveNum);
+        DisplayResult(User, squared);
+
+    }
+
+    static void DisplayWelcome(){
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName(){
+        Console.Write("What is your name? ");
+        String name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber(){
+        Console.Write("What is your favorite number? ");
+        int num = int.Parse(Console.ReadLine());
+        return num;
+    }
+
+    static int SquareNumber(int number){
+        int squared = number*number;
+        return squared;
+    }
+
+    static void DisplayResult(string firstName, int favoriteNum){
+        Console.WriteLine($"Brother {firstName}, the square of your number is {favoriteNum}");
     }
 }
